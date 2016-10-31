16756c90347d669a3c36e212f8121fdfe9610445
darkAco <lejon.doering@googlemail.com>
2016-10-31 16:08:56 +0100

    Create Parser.py

---
+++ Parser.py
@@ -0,0 +1,17 @@
+# Project and File Information
+#	File: Parser.py
+#	Project: NOAGI16
+#	https://github.com/darkAco/noagi16
+#
+
+# File Description
+#
+
+# Import-Section
+#
+
+# Class 'Parser'
+#
+class Parser(object):
+  def __init__(self):
+    pass
